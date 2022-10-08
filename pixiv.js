66bbfe7c9d07f000e438ee841ab95e8997e23258
tenswo <skt11992@daum.net>
2022-10-08 17:00:33 +0900

    1

--- pixiv.js
+++ pixiv.js
@@ -1,8 +1,18 @@
+function changeMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    }
 $(function(){
     $(".banner .slidebanner").slick();
 });
-$(function() {
-    $(".heart img").mousedown();
+// $('.banner').slick();
+document.querySelectorAll('.illust img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup_image').style.display = 'block';
+        document.querySelector('.popup_image img').src = image.getAttribute('src');
+    }
 });
 
-// $('.banner').slick();
+document.querySelector('.popup_image span').onclick = () =>{
+    document.querySelector('.popup_image').style.display = 'none';
+}
