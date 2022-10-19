a9a665deec4d859239be6b729b7da3fb60e7679b
tenswo <skt11992@daum.net>
2022-10-19 07:15:05 +0000

    123
    123

---
+++ work.js
@@ -0,0 +1,10 @@
+function changeMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    }
+$ (function() {
+    $('li').click(function() {
+        $('li').removeClass()
+            $('this').addClass('on')
+        });
+});
